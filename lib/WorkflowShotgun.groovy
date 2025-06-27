03495f048fc22b1d54e85a1193c48fc2cf7a6548
JIANHONG OU <jianhong@users.noreply.github.com>
2025-06-27 12:53:47 -0400

    Update WorkflowShotgun.groovy

--- lib/WorkflowShotgun.groovy
+++ lib/WorkflowShotgun.groovy
@@ -10,7 +10,7 @@ class WorkflowShotgun {
     // Check and validate parameters
     //
     public static void initialise(params, log) {
-        genomeExistsError(params, log)
+        //genomeExistsError(params, log)
 
         if (!params.fasta) {
             log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
